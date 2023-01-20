194fa749ae78277adabb7354964d7c6df8095c10
chehr <chehrazad.ouazzani@student.vinci.be>
2023-01-20 10:34:41 +0100

    refactor(validations): refactor of containsNumber

--- src/services/validations.js
+++ src/services/validations.js
@@ -6,11 +6,7 @@ const isEightChar = (label) => label.length >= 8;
 
 module.exports.containsSpecialChar = (label) => /[^a-zA-Z]/.test(label);
 
-module.exports.containsNumber = (label) => {
-    if (!label) return false;
-    const found = /\d/.test(label);
-    return found;
-};
+module.exports.containsNumber = (label) =>  /\d/.test(label);
 
 
 exports.isEmpty = isEmpty;
